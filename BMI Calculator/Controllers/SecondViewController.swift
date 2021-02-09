cb01d5cb4ec49ffdc3579b2c4df9caf3fc98b7b8
Dylan Park <dylanpark.design@gmail.com>
2021-02-09 16:13:16 +1000

    Create SecondViewController.swift

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Dylan Park on 9/2/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+ 
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
