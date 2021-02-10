e5dafeb2663709aa9ae948d8bfd18ce0dd922dfe
Dylan Park <dylanpark.design@gmail.com>
2021-02-10 12:19:57 +1000

    Update CalculatorBrain.swift

--- BMI Calculator/Model/CalculatorBrain.swift
+++ BMI Calculator/Model/CalculatorBrain.swift
@@ -19,15 +19,24 @@ struct CalculatorBrain {
         
     }
     
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
     mutating func calculateBMI(height: Float, weight: Float) {
         let bmiValue = weight / (height * height)
         
         if bmiValue < 18.5 {
-            bmi = BMI(value: bmiValue, advice: "Eat more pies", )
+            bmi = BMI(value: bmiValue, advice: "Eat more snakcs", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
         } else if bmiValue < 24.9 {
-            print("normal weight")
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1))
         } else {
-            print("overweight")
+            bmi = BMI(value: bmiValue, advice: "Eat less snacks!", color: #colorLiteral(red: 0.7898992529, green: 0.1339604735, blue: 0, alpha: 1))
         }
         
     }
